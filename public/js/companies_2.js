3a8178ac45645051e022789fc92332f2eb5e0ba8
Vivek Haridas <vivek.haridas@gmail.com>
2020-02-13 22:10:43 +0530

    removed used code

--- public/js/companies_2.js
+++ public/js/companies_2.js
@@ -2,19 +2,19 @@ const chartSize = { width: 800, height: 600 };
 const margin = { left: 100, right: 10, top: 20, bottom: 150 };
 const width = chartSize.width - margin.left - margin.right;
 const height = chartSize.height - margin.top - margin.bottom;
-const formats = { 
-  Rs:d=>`${d} ₹`,
-  kCrRs:d=>`${d/1000}k Cr ₹`,
-  Percent:d=>`${d}%`
-};
-const configs = [['CMP',formats.Rs],['MarketCap',formats.kCrRs],['PE'],['DivYld',formats.Percent],['ROCE',formats.Percent]];
-const updateChart = (companies, step=0) => {
-  const [fieldName,format] = configs[step%configs.length];
-  
-  const svg = d3.select('#chart-area svg');
+const Rs = d => `${d} ₹`;
+const kCrRs = d => `${d / 1000}k Cr ₹`;
+const Percent = d => `${d}%`;
+const fieldFormat = { CMP: Rs, MarketCap: kCrRs, DivYld: Percent, ROCE: Percent, QNetProfit: kCrRs, QSales: kCrRs };
+const companyNameAsId = c => c.Name;
+const slow = ()=>d3.transition().duration(1500).ease(d3.easeLinear);
+const updateChart = (companies, fieldName) => {
+  const format = fieldFormat[fieldName];
 
+  const svg = d3.select('#chart-area svg');
+  const maxDomain = _.get(_.maxBy(companies, fieldName),fieldName,0);
   const y = d3.scaleLinear()
-    .domain([0, _.maxBy(companies, fieldName)[fieldName]])
+    .domain([0, maxDomain])
     .range([height, 0]);
 
   svg.select('.y.axis-label').text(fieldName);
@@ -24,11 +24,24 @@ const updateChart = (companies, step=0) => {
     .tickFormat(format);
 
   svg.select('.y.axis').call(yAxis);
+
+  const x = d3.scaleBand()
+    .domain(_.map(companies, 'Name'))
+    .range([0, width])
+    .padding(0.3);
+
+  svg.selectAll('g rect').data(companies, companyNameAsId)
+    .exit().remove();
+
+  const xAxis = d3.axisBottom(x);
+  svg.select('.x.axis').transition(slow()).call(xAxis);
   
-  svg.selectAll('g rect').data(companies)
-    .transition().duration(2000).ease(d3.easeLinear)
+  svg.selectAll('g rect').data(companies, companyNameAsId)
+    .transition(slow())
     .attr('height', b => y(0) - y(b[fieldName]))
     .attr('y', b => y(b[fieldName]))
+    .attr('x', b=> x(b.Name))
+    .attr('width',x.bandwidth);
 }
 const initChart = (companies) => {
   const svg = d3.select('#chart-area svg')
@@ -41,7 +54,7 @@ const initChart = (companies) => {
     .padding(0.3);
 
   const color = d3.scaleOrdinal(d3.schemeCategory10);
-  const xAxis = d3.axisBottom(x);  
+  const xAxis = d3.axisBottom(x);
 
   const g = svg.append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
@@ -65,24 +78,30 @@ const initChart = (companies) => {
   g.append('g')
     .attr('class', 'y axis');
 
-  const rects = g.selectAll('rect').data(companies);
+  const rects = g.selectAll('rect').data(companies, companyNameAsId);
   const newRects = rects.enter();
   newRects.append('rect')
     .attr('x', b => x(b.Name))
-    .attr('y', height)    
-    .attr('width', x.bandwidth)    
+    .attr('y', height)
+    .attr('width', x.bandwidth)
     .attr('fill', b => color(b.Name));
-  updateChart(companies);
+
 };
 
 const parseCompany = ({ Name, ...rest }) => {
   _.forEach(rest, (v, k) => rest[k] = +v);
   return { Name, ...rest };
 }
+const nextName = (() => {
+  let step = 0;
+  const names = 'CMP,PE,MarketCap,DivYld,QNetProfit,QSales,ROCE'.split(',');
+  return () => names[step++ % names.length];
+})();
 const startVisualization = (companies) => {
-  let step = 1;
   initChart(companies);
-  setInterval(() => updateChart(companies, step++), 3000);
+  updateChart(companies, nextName());
+  setInterval(() => updateChart(companies, nextName()), 3000);
+  setInterval(() => companies.shift(), 6000);
 }
 const main = () => {
   d3.csv('data/companies.csv', parseCompany).then(startVisualization);
